3f99957cfa9a9657ea89319d1cac7cc6315960cc
TungPhamQ <tungpham2109@gmail.com>
2026-01-22 16:02:39 +0700

    fix firebase

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,21 +1,8 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
+import auth from "@react-native-firebase/auth";
+import firestore from "@react-native-firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD9H_PEKqZbiCZf7-PCCi1Iv7zg5t3VHss",
-  authDomain: "rork-tetris.firebaseapp.com",
-  projectId: "rork-tetris",
-  storageBucket: "rork-tetris.firebasestorage.app",
-  messagingSenderId: "518668919554",
-  appId: "1:518668919554:web:04bfcbe648fa7339c3d81b",
-};
+// Firebase is automatically initialized on React Native via google-services.json
+// No manual config needed
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-export const firebaseApp = firebase.app();
-export const auth = firebase.auth;
-export const firestore = firebase.firestore;
-export default firebase;
+export { auth, firestore };
+export default { auth, firestore };
